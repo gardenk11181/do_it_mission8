e2666e7ffda88170c37f430d744924c90552f838
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-01-27 15:13:49 +0900

    Layout Finish

---
+++ app/src/main/java/com/gardenlab/doitmission8/SubActivity2.java
@@ -0,0 +1,14 @@
+package com.gardenlab.doitmission8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub2);
+    }
+}
